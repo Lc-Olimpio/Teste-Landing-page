ad47046b31092cf35f3eb6ea92cf8ab7a2c99f9d
Lucas Olimpio <lucasolimpio267@gmail.com>
2022-11-22 23:50:41 -0300

    checando inputs

--- script/script.js
+++ script/script.js
@@ -11,17 +11,19 @@ Como eu ainda não sei quais os métodos mais comuns de trabalhar com JS vou me 
 const Main = {
 
   init: function() {
-    console.log(this)
+    
     this.cacheSelectors()
     this.bindEvents()
   },
 
   cacheSelectors: function() {
     this.$form1 = document.forms[0]
+    this.$form2 = document.forms[1]
   },
 
   bindEvents: function() {
     this.$form1.onsubmit = this.Events.inputCheck
+    this.$form2.onsubmit = this.Events.inputCheck2
   },
 
   Events : {
@@ -29,27 +31,107 @@ const Main = {
     inputCheck : function(e) {
       e.preventDefault()
 
-      let form1 = document.forms[0]
-      let nameInput = form1.name
-      let emailInput = form1.email
-      let cpfInput= form1.cpf
+      const form1 = document.forms[0]
+      const nameInput = form1.name
+      const emailInput = form1.email
+      const cpfInput= form1.cpf
+      const genderInput1 = document.querySelector('#male').checked
+      const genderInput2 = document.querySelector('#female').checked
 
-      console.log(nameInput)
-      console.log(emailInput)
-      console.log(cpfInput)
-      
-      if (nameInput.value == ''){
-        console.log(this)
-        this.inputError() // Como seria se o this funcionasse como funciona no 'Main'
+      nameInputCheck()
+      emailInputCheck()
+      cpfInputCheck()
 
-        /* Meu objetivo era fazer uma função separada para adicionar as classes de "erro" porém eu não sei como navegar até essa função aqui de baixo 'inputError' já que o 'this' não é o Events aqui.*/
+
+      if (nameInputCheck() == true && emailInputCheck() == true && cpfInputCheck() == true){
+
+        if (genderInput1 == true || genderInput2 == true) {
+          form1.submit()
+        }     
+      }  
+
+      function nameInputCheck() {
+        
+        function stringContainsNumber(_string) {
+          return /\d/.test(_string);
+        }
+
+        let containnumber = stringContainsNumber(nameInput.value)? true:false
+        
+        if (nameInput.value == '' || containnumber == true){
+          nameInput.classList.add('inputError')
+          return false
+        }
+        nameInput.classList.remove('inputError')
+        return true
+      }
+
+      function emailInputCheck() {
+        
+        if (emailInput.value == '' || emailInput.value.includes('@') == false || emailInput.value.includes('.com') == false)  {
+
+          emailInput.classList.add('inputError')
+          return false
+        }
+        emailInput.classList.remove('inputError')
+        return true
+      }
+
+      function cpfInputCheck() {
+        
+        const isnum = /^\d+$/.test(cpfInput.value)
+        
+        if (cpfInput.value == '' || isnum == false)  {
+
+          cpfInput.classList.add('inputError')
+          return false
+        }
+        cpfInput.classList.remove('inputError')
+        return true
       }
     },
 
-    inputError : function() {
-      // funcão que colocaria as clases de erros nos inputs.
-    }
+    inputCheck2 : function(e) {
+      e.preventDefault()
 
+      const form2 = document.forms[1]
+      const nameInput = form2.name
+      const emailInput = form2.email
+
+      nameInputCheck()
+      emailInputCheck()
+
+      if (nameInputCheck() == true && emailInputCheck() == true){
+        form2.submit()
+      }  
+
+      function nameInputCheck() {
+        
+        function stringContainsNumber(_string) {
+          return /\d/.test(_string);
+        }
+
+        let containnumber = stringContainsNumber(nameInput.value)? true:false
+        
+        if (nameInput.value == '' || containnumber == true){
+          nameInput.classList.add('inputError')
+          return false
+        }
+        nameInput.classList.remove('inputError')
+        return true
+      }
+
+      function emailInputCheck() {
+        
+        if (emailInput.value == '' || emailInput.value.includes('@') == false || emailInput.value.includes('.com') == false)  {
+
+          emailInput.classList.add('inputError')
+          return false
+        }
+        emailInput.classList.remove('inputError')
+        return true
+      }
+    }
   }
 }
 
